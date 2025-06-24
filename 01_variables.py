6ac54905658fd08fb6a89bca9e38c003a12b6965
JUNAID QURESHI <mohammedjunaid1310@gmail.com>
2025-06-24 15:52:08 +0530

    add more progress

---
+++ 01_variables.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+print(a + b)
+
+# three case in variables
+# Camel Case - myVariableName
+# Pascal case - MyVariableName
+# snake case - my_variable_name
+a = "Hello"
+print(a[4])
+print('Hello', 'World')
--- module.py
+++ module.py
@@ -1,21 +1,24 @@
+import random
 import os
 import pyttsx3
 import pyjokes
 import sys
+
+# Jokes module--------------
+# import pyjokes
 print(sys.version)
 joke = pyjokes.get_joke()
-print(joke)
+print("jokes---->",joke)
 
 # Q.3 install an external module and use it to perform an operation of your interest?
-
+#Text To Speech ----------------------
 # import pyttsx3 
 engine = pyttsx3.init()
 # engine.say("I will speak this text")
 engine.say("Hello JUNAID sir")
 engine.runAndWait()
 
-# Q.4 WAP to print the contents of a directory using the os module,Search online for the function which does that?
-
+# Q.4 WAP to print the contents of a directory using the os module,Search online for the function which does that?----------------
 
 # import os
 # specify the directory you want to list
@@ -28,3 +31,8 @@ print("Contents of the directory:")
 for item in contents:
     print(item)
  
+
+# Import the random module, and display a random number from 1 to 9:-------------------
+
+# import random
+print("random number ----->",random.randrange(1, 10))
