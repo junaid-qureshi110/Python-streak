74d0c07cf04ca3058bb21ca9dad5c950b148b228
JUNAID QURESHI <mohammedjunaid1310@gmail.com>
2025-06-25 17:46:06 +0530

    add more files

--- problem_02.py
+++ problem_02.py
@@ -27,3 +27,4 @@ print("The square of the number is", a*a)
 
 # print("The square of the number is", a^2) # Incorrect for finding square of a number in Python
  
+ 
---
+++ str_functions.py
@@ -0,0 +1,3 @@
+name = "JunaiD"
+ print(len(name))
+ print(name)
